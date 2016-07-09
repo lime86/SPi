cd6601f2e1a866d165a12de3cda9063513853ce2
Lingxin Meng <lingxin.meng@cern.ch>
2016-07-09 12:13:34 +0200

    run.py: token, process (re)start and termination
    - Added chmod for token file
    - Added function to start surveillance script + check time stamp, and process termination, restart

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-import os.path
+import os
+import signal
 import shutil
+import psutil
 import subprocess
 import datetime
 import time
@@ -15,6 +17,16 @@ tokenpath = conf["tokenpath"]
 tmptokenpath = conf["tmptokenpath"]
 tmptimepath = conf["tmptimepath"]
 
+#if os.path.exists(tmptokenpath):
+	#os.chmod(tmptokenpath, 0666)
+
+pid = 0
+
+def start_surveillance():
+	print datetime.datetime.now(), "Starting surveillance..."
+	surveillance = subprocess.Popen(["/home/pi/.virtualenvs/cv/bin/python", "/home/pi/SPi/pi_surveillance.py", "--conf", "/home/pi/SPi/conf.json"])
+	return surveillance.pid
+
 if os.path.exists("/dev/video0"):
 	print "Camera connected."
 else:
@@ -24,12 +36,15 @@ else:
 if __name__ == "__main__":
 	if conf["use_dropbox"]:
 		if os.path.isfile(tokenpath):
-			shutil.copy(tokenpath, "/tmp/")
+			if not os.path.exists(tmptokenpath):
+				shutil.copy(tokenpath, tmptokenpath)
+				os.chmod(tmptokenpath, 0666)
 		else:
 			print "No token."
 			quit()
-	print "Starting surveillance..."		
-	subprocess.Popen(["/home/pi/.virtualenvs/cv/bin/python", "/home/pi/SPi/pi_surveillance.py", "--conf", "/home/pi/SPi/conf.json"])
+	pid = start_surveillance()
+	time.sleep(conf["camera_warmup_time"])
+#	print "Process PID: ", pid
 
 while True:
 	if os.path.isfile(tmptimepath):
@@ -40,19 +55,44 @@ while True:
 			else:
 				lasttime = datetime.datetime.strptime(line, '%Y-%m-%d %H:%M:%S.%f')
 				thistime = datetime.datetime.now()
-				print lasttime
-				print thistime
-				if (thistime - lasttime).seconds > conf["check_seconds"]:
-					print "> 10"
-				elif (thistime - lasttime).seconds < conf["check_seconds"]:
-					print "< 10"
-				else:
-					print "haha"
-		time.sleep(conf["check_seconds"])
+		
+				## check if the script has been writing regularly to a file
+				
+				if (thistime - lasttime).seconds > conf["read_seconds"]:
+					print "Idle > ", conf["read_seconds"], "PID: ", pid
+					p = psutil.Process(pid)
+					try:
+						p.terminate()
+						p.wait(timeout=1)
+						#os.kill(pid, signal.SIGTERM)
+						#os.kill(pid, signal.SIGKILL)
+					except OSError:
+						raise
+				
+					if pid not in psutil.pids():
+						pid = start_surveillance()
+					else:
+						p.kill()
+						p.wait(timeout=1)
+						continue
+						
+				#elif (thistime - lasttime).seconds < conf["read_seconds"]:
+					#print "< ", conf["read_seconds"]
+					#pass
+				#else:
+					#print "Time difference: ", (thistime - lasttime).seconds
+					#pass				
+		time.sleep(conf["read_seconds"])
+		
 	else:
+		time.sleep(conf["read_seconds"])
 		print "No time file. Restart surveillance."
-		#implement the restarting thing
-		quit()
+		psutil.Process(pid).terminate()
+		psutil.Process(pid).wait(timeout=1)
+		if not pid in psutil.pids():
+			pid = start_surveillance()
+
+		continue
 				
 		
 
